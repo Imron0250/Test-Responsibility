97510e1cc1e90ab6b2bcff9f4906521b5fa07317
Imronbek0250 <sultanovimron0@gmail.com>
2024-07-01 19:06:06 +0500

    New addings

--- obj/Debug/net8.0/myproject.AssemblyInfo.cs
+++ obj/Debug/net8.0/myproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68898bcff7e1ce50424eb1e09b47e149c27e2312")]
 [assembly: System.Reflection.AssemblyProductAttribute("myproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
