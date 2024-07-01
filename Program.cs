685a87e317fbf46b743f371d5331f3414945e5e2
Imron0250 <124577347+Imron0250@users.noreply.github.com>
2024-07-01 21:46:40 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MyProject
 
             
             Console.WriteLine("Hello, World!");
-            Console.Write("How are you");
+            Console.Write("Whats up ");
             Console.ReadLine();
             
             
